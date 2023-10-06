6ee7203d82ff760c7c94e33d547ad7ea6a4f0ec2
Estefania Amundaray <estefania.amundaray@emburse.com>
2023-10-06 16:10:15 +0200

    refactored function and typescript enforced

--- index.ts
+++ index.ts
@@ -12,10 +12,15 @@ axios.get(url).then((response) => {
 
   const ID = todo.id;
   const title = todo.title;
-  const finished = todo.completed;
-  console.log(`
-  The Todo with ID: ${ID}
-  Has a title of: ${title}
-  Finished? ${finished}
-  `);
+  const completed = todo.completed;
+
+  logTodo(ID, title, completed);
 });
+
+const logTodo = (id: number, title: string, completed: boolean) => {
+  console.log(`
+    The Todo with ID: ${id}
+    Has a title of: ${title}
+    Finished? ${completed}
+    `);
+};
